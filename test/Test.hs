7547db0b0a40e37b5f170ce708ff599d58f32c14
Dominic Steinitz <dominic@steinitz.org>
2019-06-12 15:33:04 +0100

    Add a cabal file and restructure

---
+++ test/Test.hs
@@ -0,0 +1,12 @@
+module Main (main) where
+
+import Graphics.Rendering.Chart
+import Graphics.Rendering.Chart.Backend.Diagrams
+import Diagrams.Backend.Cairo.CmdLine
+import Diagrams.Prelude hiding ( render, Renderable, (*~), Time )
+import Diagrams.Backend.CmdLine
+
+import ChebyshevApproximations
+
+main :: IO ()
+main = return ()
