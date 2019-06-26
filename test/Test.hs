63a4729888edfda8927da89eb58f3f2401fb4364
deifilia <deifilia@ubuntu-18.04>
2019-06-26 21:39:34 +0200

    plotting residuals

--- test/Test.hs
+++ test/Test.hs
@@ -6,9 +6,11 @@ import Diagrams.Backend.Cairo.CmdLine
 import Diagrams.Prelude hiding ( render, Renderable, (*~), Time )
 import Diagrams.Backend.CmdLine
 
+
 import System.IO.Unsafe
 
 import ChebyshevApproximations
+import ChebTyping
 
 denv :: DEnv Double
 denv = unsafePerformIO $ defaultEnv vectorAlignmentFns 600 500
@@ -52,5 +54,16 @@ diag :: String ->
 diag t xss =
  fst $ runBackend denv (render (chart t xss) (600, 500))
 
-main :: IO ()
-main = return ()
+main = 
+       let 
+              x = ChebTyping.x 
+              f = fromCheb (ChebTyping.cos x)
+              cheb = map (\x -> (polCalc f x 0)) [-1, -0.99..1]
+              residual = map (\x -> (polCalc f x 0) Prelude.- (Prelude.cos x)) [-1, -0.99..1] 
+              cheb20 = chebf Prelude.cos 20
+              cheb20plot = map (\x -> (polCalc (cheb20) x 0) Prelude.- Prelude.cos x ) [-1, -0.99..1]
+       in 
+       do
+              renderableToFile def "cos-cheb" (chart "cos-cheb" ([zip [-1, -0.99..1] cheb]))
+              renderableToFile def "cos-residual" (chart "cos-residual" ([zip [-1, -0.99..1] residual]))
+              renderableToFile def "cos-residual-n=20" (chart "cos-residual-n=20" ([zip [-1, -0.99..1] cheb20plot]))
